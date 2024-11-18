d1165a523eb822b01b00eb0cbfe2f4cb9829f568
Aelderian <simikrefero@gmail.com>
2024-11-18 10:21:33 +0100

    Testing

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -13,20 +13,35 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', // Use babel-loader to transpile JS/JSX files
+          loader: 'babel-loader', // Transpile JS/JSX files using Babel
         },
       },
       {
-        test: /\.svg$/, // Add rule to handle SVG files
+        test: /\.svg$/, // Rule for SVG files
         use: [
           {
-            loader: '@svgr/webpack', // Converts SVGs to React components
+            loader: '@svgr/webpack', // Convert SVG to React components
             options: {
               // Optional configuration for svgr
             },
           },
-          'url-loader', // This will handle the SVG as a URL (optional)
+          'url-loader', // This is optional and allows you to handle SVG as a URL
         ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|svg)$/i, // Rule for image files
+        use: [
+          {
+            loader: 'file-loader', // Handles image files and copies them to the output folder
+            options: {
+              name: '[path][name].[ext]', // Keep the original path and name
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i, // Rule for CSS files
+        use: ['style-loader', 'css-loader'], // Inject styles and parse CSS files
       },
     ],
   },
