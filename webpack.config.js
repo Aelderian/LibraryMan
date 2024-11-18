dff43132675f85ee22ebf2be6896b6a97f03ef04
Aelderian <simikrefero@gmail.com>
2024-11-18 10:01:21 +0100

    Testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 export default {
   mode: 'development', // or 'production'
-  entry: './src/main.js',
+  entry: './src/main.jsx',
   output: {
     filename: 'bundle.js',
     path: new URL('./dist', import.meta.url).pathname
